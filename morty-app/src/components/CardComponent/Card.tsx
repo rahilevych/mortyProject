75d616b1f53b0175933c4e2fcd327d6444bd0f69
AlonaRahilevych <ragilevic@gmail.com>
2024-05-06 11:34:42 +0200

    flip feature was added to cards

--- morty-app/src/components/CardComponent/Card.tsx
+++ morty-app/src/components/CardComponent/Card.tsx
@@ -1,13 +1,21 @@
 import { FC } from 'react';
 import { Cards } from '../../../assets/Types';
-import './Card.css';
+import './Card.scss';
 interface CardProps {
   data: Cards;
 }
 const Card: FC<CardProps> = ({ data }) => {
   return (
-    <div className='card'>
-      <img src={data.image} alt='' className='card__img' />
+    <div className='flip-card'>
+      <div className='flip-card-inner'>
+        <div className='flip-card-front'>
+          <img src={data.image} alt='' />
+        </div>
+        <div className='flip-card-back'>
+          <h1>{data.name}</h1>
+          <button>LEAR MORE</button>
+        </div>
+      </div>
     </div>
   );
 };
